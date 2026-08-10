874b7d9c554a2981ddfaaea6a42324f77002aa81
yaywiin <yaywiin@github.com>
2026-08-10 11:11:40 -0600

    style: remover texto de ayuda de Paises Seleccionados

--- admin/src/views/Envio.vue
+++ admin/src/views/Envio.vue
@@ -111,12 +111,11 @@
                       <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
                     </button>
                   </span>
-                  <span v-if="nuevaRegla.paises.length === 0" class="text-xs text-gray-400">
-                    Ningún país seleccionado. Selecciona una opción en los desplegables superiores.
-                  </span>
                 </div>
               </div>
             </div>
+
+
 
 
             <div v-if="nuevaRegla.paises.includes('México') || nuevaRegla.paises.includes('Mexico')">
